d0ddb8699d5f74a1882cb15f8eef60837153107d
SoundariyaPalanivel <palanivelsoundariya@gmail.com>
2023-11-25 19:33:27 +0530

    First time push my code

---
+++ src/test/java/org/demo/ClassA.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class ClassA {
+
+	private void m1() {
+		System.out.println("m1");
+	}
+
+	private void m2() {
+		System.out.println("m2");
+	}
+
+	private void m3() {
+		System.out.println("m3");
+	}
+
+}
